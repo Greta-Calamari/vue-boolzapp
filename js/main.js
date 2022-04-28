5fd1ca451683624699a26abfd40537c9c1aa97d6
ladykakki <greta_calamari@hotmail.it>
2022-04-28 12:09:58 +0200

    fix dropdown finalmente

--- js/main.js
+++ js/main.js
@@ -9,22 +9,22 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -37,22 +37,22 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+                        
                         show:false,
-
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -65,15 +65,15 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        
                         show:false,
-
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
@@ -93,15 +93,15 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '15/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '15/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -114,15 +114,15 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '26/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '26/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -135,22 +135,22 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '13/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '13/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        
                         show:false,
-
                         date: '13/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -163,15 +163,15 @@ const boolzapp = new Vue({
                 visible: true,
                 messages: [
                     {
+                        
                         show:false,
-
                         date: '29/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '29/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -190,15 +190,15 @@ const boolzapp = new Vue({
                         status: 'received'
                     },
                     {
+                        
                         show:false,
-
                         date: '11/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
+                        
                         show:false,
-
                         date: '11/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
